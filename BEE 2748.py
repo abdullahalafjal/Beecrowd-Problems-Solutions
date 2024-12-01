0e52f86670e761bbd7dbb5459ae6cab9f45f966c
Abdullah Al Afjal <159076048+abdullahalafjal@users.noreply.github.com>
2024-12-02 02:49:34 +0600

    Add files via upload

---
+++ BEE 2748.py
@@ -0,0 +1,15 @@
+horizontal_line = "-" * 39
+
+line1 = "|        Roberto                      |"
+line2 = "|                                     |"
+line3 = "|        5786                         |"
+line4 = "|                                     |"
+line5 = "|        UNIFEI                       |"
+
+print(horizontal_line)
+print(line1)
+print(line2)
+print(line3)
+print(line4)
+print(line5)
+print(horizontal_line)
